1d0d022418703c99d4bff87b44fb70a61b61845b
Benjamin <Benjamin>
2017-07-26 17:05:40 +0800

    first commit

---
+++ stringtool.go
@@ -0,0 +1,27 @@
+//Package stringtool is a colllection of functions to manipulate string
+package stringtool
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"regexp"
+	"strings"
+)
+
+var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
+var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
+
+//ToSnakeCase generate string name from camel case (asdQwe) to snake case (asd_qwe)
+func ToSnakeCase(str string) string {
+	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
+	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
+	return strings.ToLower(snake)
+}
+
+//MakeMD5 generate MD5 check summary from string (32 charactor long)
+func MakeMD5(value string) string {
+	md5 := md5.New()
+	md5.Write([]byte(value))
+
+	return hex.EncodeToString(md5.Sum(nil))
+}
